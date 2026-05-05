cf7338b95cc14e7847bf1c7140cdac0e3a4a1d1d
Yasmim <yangalonga@gmail.com>
2026-05-05 13:21:09 -0300

    atualizando

---
+++ app.js
@@ -0,0 +1,28 @@
+alert("Boas vindas ao jogo do número secreto");
+let numeroSecreto = 5;
+let chute = prompt("Escolha um número entre 1 e 10");
+//se chute for igual ao numero secreto
+if(chute == numeroSecreto) {
+    console.log("Isso aí! Você descobriu o nùmero secreto (5) ");
+} else {
+    alert("Você errou :(")
+}
+ 
+// Desafios:
+
+// alert("Boas vindas ao nosso site!");
+// let nome = "Lua";
+// let idade = 25;
+// let numeroDevendas = 50;
+// let saldoDisponivel = 1000;
+// alert("Erro! Preencha todos os campos");
+// let mensagemDeErro = "Erro! Preencha todos os campos";
+// alert (mensagemDeErro);
+ 
+// let nomeUsuario = prompt ("Qual é o seu nome?");
+// let idade = prompt ("digite a sua idade:");
+
+
+// if(idade >= 18) {
+//     console.log("Pode tirar a habilitação!");
+// }

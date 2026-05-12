80ab42b36e5fbe092e9de501406d1dc94e3e350f
Yasmim <kimyasmim56@gmail.com>
2026-05-12 18:16:37 -0300

    salvar os codigos novos

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 5;
+let numeroSecreto = 4;
+console.log(numeroSecreto);
 let chute = prompt("Escolha um número entre 1 e 10");
 //se chute for igual ao numero secreto
 if(chute == numeroSecreto) {
-    console.log("Isso aí! Você descobriu o nùmero secreto (5) ");
+    alert(`Isso aí! Você descobriu o nùmero secreto ${numeroSecreto}`);
 } else {
     alert("Você errou :(")
 }

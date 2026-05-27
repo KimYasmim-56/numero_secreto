1eaadb17aee35283993aaa9b40e61f7f0f60c5c4
Yasmim <kimyasmim56@gmail.com>
2026-05-26 21:00:03 -0300

    enquanto

--- app.js
+++ app.js
@@ -5,20 +5,25 @@ let numeroSecreto = 5;
 // console.log('Valor do chute:', chute);
 
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute
+
 // um console.log para verificar a comparação entre "chute" e "numeroSecreto"
 // console.log('Resultado da comparação:', chute == numeroSecreto);
 
-//se chute for igual ao numero secreto
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o nùmero secreto ${numeroSecreto}`);
-} else {
+// enquanto o chute não for igual ao numero secreto 
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    //se chute for igual ao numero secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o nùmero secreto ${numeroSecreto}`);
+    } else {
     // Adicione um console.log para verificar o valor de "numeroSecreto" quando o jogador erra
     // console.log('Valor do número secreto:', numeroSecreto);
     // alert('O número secreto era ' + numeroSecreto);
-    if (chute > numeroSecreto) {
-        alert (`O número secreto é menor que ${chute}`);
-    } else {
-        alert (`O número secreto é maior que ${chute}`);
+        if (chute > numeroSecreto) {
+            alert (`O número secreto é menor que ${chute}`);
+        } else {
+            alert (`O número secreto é maior que ${chute}`);
+        }
     }
 }

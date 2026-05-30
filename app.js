0d9393f6ba1e93833aef27d2b9d77df3578529b4
Yasmim <kimyasmim56@gmail.com>
2026-05-29 21:05:42 -0300

    número aleatório (gerar)

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 
 // um console.log para verificar o valor de "chute" após a entrada do usuário
 // console.log('Valor do chute:', chute);
@@ -12,7 +12,7 @@ let tentativas = 1;
 
 // enquanto o chute não for igual ao numero secreto 
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt('Escolha um número entre 1 e 100');
     //se chute for igual ao numero secreto
     if (chute == numeroSecreto) {
 
@@ -34,10 +34,15 @@ while (chute != numeroSecreto) {
         tentativas++
     }
 }
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Você descobriu o nùmero secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
  //pq colocar "com ${tentativas} tentativas`);" na linha ?
         //Os valores das variáveis substituem aquele ${} automaticamente. É uma forma bem prática de misturar texto com valores de variáveis.
-if(tentativas > 1) {
-    alert(`Isso aí! Você descobriu o nùmero secreto ${numeroSecreto} com ${tentativas} tentativas`);
-}else {
-    alert(`Isso aí! Você descobriu o nùmero secreto ${numeroSecreto} com ${tentativas} tentativa`);
-}
+
+//ñ vomos usar por enquanto 
+// if(tentativas > 1) {
+//     alert(`Isso aí! Você descobriu o nùmero secreto ${numeroSecreto} com ${tentativas} tentativas`);
+// }else {
+//     alert(`Isso aí! Você descobriu o nùmero secreto ${numeroSecreto} com ${tentativas} tentativa`);
+// } 
